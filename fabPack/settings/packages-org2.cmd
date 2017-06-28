dfbdd4207805141103eb41de61086f581d63effe
Fabrice Cathala <fcathala@users.noreply.github.com>
2017-06-28 21:59:27 +0100

    Package names for org 2
    This file is an example to get you started and contains fake data.

---
+++ fabPack/settings/packages-org2.cmd
@@ -0,0 +1,17 @@
+@Echo OFF
+
+REM ===============================================================
+	REM CHANGE SETS NAMES
+
+	REM Avoid Spaces in Change Set names, use "-" instead
+	REM Avoid any other special character
+REM ===============================================================
+
+REM ===============================================================
+	Set PKG1=MyApp
+	Set PKG2=HotFix
+	Set PKG3=Reports
+	Set PKG4=
+	Set PKG5=
+	Set PKG6=
+REM ===============================================================
