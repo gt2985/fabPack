950eb5fc86a36235ed82781a758117e396b56610
Fabrice Cathala <fcathala@users.noreply.github.com>
2017-06-28 22:01:25 +0100

    Package names for org 3
    This file is an example to get you started and contains fake data.

---
+++ fabPack/settings/packages-org3.cmd
@@ -0,0 +1,15 @@
+REM ===============================================================
+	REM CHANGE SETS NAMES
+	
+	REM Avoid Spaces in Change Set names, use "-" instead
+	REM Avoid any other special character
+REM ===============================================================
+				
+REM ===============================================================
+	Set PKG1=
+	Set PKG2=
+	Set PKG3=
+	Set PKG4=
+	Set PKG5=
+	Set PKG6=
+REM ===============================================================
