d3b2bc0c6f2d0d5b62f2636418772d466e642586
Fabrice Cathala <fcathala@gmail.com>
2017-06-29 14:58:12 +0100

    Bugfix
    - Support for spaces in Ant install folder

--- fabPack/bin/sys/menu_action.cmd
+++ fabPack/bin/sys/menu_action.cmd
@@ -13,13 +13,13 @@
 REM ===============================================================
 :Describe
 	REM Run the task
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" describe -Ddir="%DSC%" -l "%LOG%\describe.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" describe -Ddir="%DSC%" -l "%LOG%\describe.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
 
-	REM Take a versioned snapshot and store in folder "archives"
+	REM Take a versioned snapshot and store in folder "archives" 
 	CALL "%SYS%\c1\get.cmd"
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%DSC%" -Ddst="%ARC%\%COUNTER%-describe\describe" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-describe\log" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%DSC%" -Ddst="%ARC%\%COUNTER%-describe\describe" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-describe\log" -q
 	CALL "%SYS%\c1\inc.cmd"
 
 	REM Execution result
@@ -33,13 +33,13 @@ REM ===============================================================
 REM ===============================================================
 :RetrievePKG
 	REM Run the task
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" retrievePKG -Ddir="%SRC%" -Dsf.pkg=%PKG% -l "%LOG%\retrievePKG.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" retrievePKG -Ddir="%SRC%" -Dsf.pkg=%PKG% -l "%LOG%\retrievePKG.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
 
-	REM Take a versioned snapshot and store in folder "archives"
+	REM Take a versioned snapshot and store in folder "archives" 
 	CALL "%SYS%\c1\get.cmd"
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%SRC%" -Ddst="%ARC%\%COUNTER%-retrievePKG-%PKG%\src" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-retrievePKG-%PKG%\log" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%SRC%" -Ddst="%ARC%\%COUNTER%-retrievePKG-%PKG%\src" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-retrievePKG-%PKG%\log" -q
 	CALL "%SYS%\c1\inc.cmd"
 
 	REM Execution result
@@ -53,13 +53,13 @@ REM ===============================================================
 REM ===============================================================
 :RetrieveXML
 	REM Run the task
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" retrieveXML -Ddir="%SRC%" -Dxml="%RTV%\package.xml" -l "%LOG%\retrieveXML.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" retrieveXML -Ddir="%SRC%" -Dxml="%RTV%\package.xml" -l "%LOG%\retrieveXML.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
 
-	REM Take a versioned snapshot and store in folder "archives"
+	REM Take a versioned snapshot and store in folder "archives" 
 	CALL "%SYS%\c1\get.cmd"
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%SRC%" -Ddst="%ARC%\%COUNTER%-retrieveXML\src" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-retrieveXML\log" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%SRC%" -Ddst="%ARC%\%COUNTER%-retrieveXML\src" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-retrieveXML\log" -q
 	CALL "%SYS%\c1\inc.cmd"
 
 	REM Execution result
@@ -73,12 +73,12 @@ REM ===============================================================
 REM ===============================================================
 :CheckOnly
 	REM Run the task
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" checkOnly -Ddir="%SRC%" -l "%LOG%\checkOnly.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" checkOnly -Ddir="%SRC%" -l "%LOG%\checkOnly.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
 
-	REM Take a versioned snapshot and store in folder "archives"
+	REM Take a versioned snapshot and store in folder "archives" 
 	CALL "%SYS%\c1\get.cmd"
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-checkOnly\log" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-checkOnly\log" -q
 	CALL "%SYS%\c1\inc.cmd"
 
 	REM Execution result
@@ -92,12 +92,12 @@ REM ===============================================================
 REM ===============================================================
 :Deploy
 	REM Run the task
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" deploy -Ddir="%SRC%" -l "%LOG%\deploy.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" deploy -Ddir="%SRC%" -l "%LOG%\deploy.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
 
 	REM Take a versioned snapshot and store in folder "archives"
 	CALL "%SYS%\c1\get.cmd"
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-deploy\log" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-deploy\log" -q
 	CALL "%SYS%\c1\inc.cmd"
 
 	REM Execution result
@@ -111,12 +111,12 @@ REM ===============================================================
 REM ===============================================================
 :Delete
 	REM Run the task
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" delete -Ddir="%DEL%" -l "%LOG%\delete.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" cleanup -Ddir="%LOG%" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" delete -Ddir="%DEL%" -l "%LOG%\delete.log" -Dsf.usr=%USR% -Dsf.pwd=%PWD% -Dsf.url=%URL%
 
-	REM Take a versioned snapshot and store in folder "archives"
+	REM Take a versioned snapshot and store in folder "archives" 
 	CALL "%SYS%\c1\get.cmd"
-	CALL %ANT_HOME%\bin\ant.bat -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-delete\log" -q
+	CALL "%ANT_HOME%\bin\ant.bat" -buildfile "%ANT%" archive -Dsrc="%LOG%" -Ddst="%ARC%\%COUNTER%-delete\log" -q
 	CALL "%SYS%\c1\inc.cmd"
 
 	REM Execution result
